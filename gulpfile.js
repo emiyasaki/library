544b8e062ab23a0ace30b1afd802135734c5ed19
Edmilson Miyasaki <edmilson.miyasaki@ilegra.com>
2016-01-06 17:29:37 -0200

    creating gulp task for jshint

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,10 @@ var jscs = require('gulp-jscs');
 var jsFiles = ['*.js', 'src/**/*.js'];
 
 gulp.task('style', function(){
-    gulp.src(jsFiles)
-        .pipe()
+    return gulp.src(jsFiles)
+        .pipe(jshint)
+        .pipe(jshint.reporter('jshint-stylish', {
+            verbose: true
+        }))
+        .pipe(jscs());
 });
