86d0622f991b460a2bfefe4f35c3e67e67a1d5d9
Edmilson Miyasaki <edmilson.miyasaki@ilegra.com>
2016-01-06 18:23:30 -0200

    working with nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var nodemon = require('gulp-nodemon');
 
 var jsFiles = ['*.js', 'src/**/*.js'];
 
 gulp.task('style', function(){
     return gulp.src(jsFiles)
-        .pipe(jshint)
+        .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish', {
             verbose: true
         }))
@@ -32,4 +33,20 @@ gulp.task('inject', function(){
         .pipe(wiredep(options))
         .pipe(inject(injectSrc, injectOptions))
         .pipe(gulp.dest('./src/views'));
+});
+
+gulp.task('serve', ['style', 'inject'], function(){
+    var options = {
+        script: 'app.js',
+        delayTime: 1,
+        env: {
+            'PORT': 3000
+        },
+        watch: jsFiles
+    };
+    
+    return nodemon(options)
+        .on('restart', function(ev){
+            console.log('Restarting...');
+        });
 });
