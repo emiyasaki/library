abf3adc7e7e3fced6adbed5932ccb9000f77bd21
Edmilson Miyasaki <edmilson.miyasaki@ilegra.com>
2016-01-07 17:26:48 -0200

    Router functions

--- app.js
+++ app.js
@@ -3,13 +3,18 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 5000;
-//var bookRouter = require('./src/routes/bookRoutes');
+
+var nav = [
+    { Link: '/Books', Text: 'Books' },
+    { Link: '/Authors', Text: 'Authors' }
+];
+var bookRouter = require('./src/routes/bookRoutes')(nav);
 
 app.use(express.static('public'));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
-app.use('/Books', require('./src/routes/bookRoutes'));
+app.use('/Books', bookRouter);
 
 app.get('/', function (req, res) {
     res.render('index',
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -2,48 +2,46 @@ var express = require('express');
 
 var bookRouter = express.Router();
 
-var books = [
-    {
-        title: 'War and Peace',
-        genre: 'Historical Fiction',
-        author: 'Tolstoy',
-        read: false
-    },
-    {
-        title: 'Les Misérables',
-        genre: 'Historical Fiction',
-        author: 'Victor Hugo',
-        read: true
-    }
-];
+var router = function (nav) {
+    var books = [
+        {
+            title: 'War and Peace',
+            genre: 'Historical Fiction',
+            author: 'Tolstoy',
+            read: false
+        },
+        {
+            title: 'Les Misérables',
+            genre: 'Historical Fiction',
+            author: 'Victor Hugo',
+            read: true
+        }
+    ];
 
-bookRouter.route('/')
-    .get(function (req, res) {
-        res.render('bookListView',
-            {
-                title: 'Books',
-                nav: [
-                    { Link: '/Books', Text: 'Books' },
-                    { Link: '/Authors', Text: 'Authors' }
-                ],
-                books: books
-            }
-        );
-    });
+    bookRouter.route('/')
+        .get(function (req, res) {
+            res.render('bookListView',
+                {
+                    title: 'Books',
+                    nav: nav,
+                    books: books
+                }
+                );
+        });
 
-bookRouter.route('/:id')
-    .get(function (req, res) {
-        var id = req.params.id;
-        res.render('bookView',
-            {
-                title: books[id].title,
-                nav: [
-                    { Link: '/Books', Text: 'Books' },
-                    { Link: '/Authors', Text: 'Authors' }
-                ],
-                book: books[id]
-            }
-        );
-    });
+    bookRouter.route('/:id')
+        .get(function (req, res) {
+            var id = req.params.id;
+            res.render('bookView',
+                {
+                    title: books[id].title,
+                    nav: nav,
+                    book: books[id]
+                }
+                );
+        });
+    return bookRouter;
+};
 
-module.exports = bookRouter;
+
+module.exports = router;
