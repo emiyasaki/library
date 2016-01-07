3229178aa901bf116179295fed3e108c0fba658f
Edmilson Miyasaki <edmilson.miyasaki@ilegra.com>
2016-01-07 16:58:36 -0200

    separating routes to specific files

--- app.js
+++ app.js
@@ -3,46 +3,13 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 5000;
-var bookRouter = express.Router();
+//var bookRouter = require('./src/routes/bookRoutes');
 
 app.use(express.static('public'));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
-var books = [
-    {
-        title: 'War and Peace',
-        genre: 'Historical Fiction',
-        author: 'Tolstoy',
-        read: false
-    }, 
-    {
-        title: 'Les Misérables',
-        genre: 'Historical Fiction',
-        author: 'Victor Hugo',
-        read: true
-    }
-];
-bookRouter.route('/')
-    .get(function (req, res) {
-        res.render('books', 
-        {
-            title: 'Books',
-            nav: [
-                { Link: '/Books', Text: 'Books' },
-                { Link: '/Authors', Text: 'Authors' }
-            ],
-            books: books
-        }
-        );
-    });
-
-bookRouter.route('/single')
-    .get(function (req, res) {
-        res.send('Hello Single Book');
-    });
-
-app.use('/Books', bookRouter);
+app.use('/Books', require('./src/routes/bookRoutes'));
 
 app.get('/', function (req, res) {
     res.render('index',
@@ -53,10 +20,6 @@ app.get('/', function (req, res) {
                 { Link: '/Authors', Text: 'Authors' }
             ]
         });
-});
-
-app.get('/books', function (req, res) {
-    res.send('Hello, books');
 });
 
 app.listen(port, function (err) {
---
+++ src/routes/bookRoutes.js
@@ -0,0 +1,39 @@
+var express = require('express');
+
+var bookRouter = express.Router();
+
+var books = [
+    {
+        title: 'War and Peace',
+        genre: 'Historical Fiction',
+        author: 'Tolstoy',
+        read: false
+    },
+    {
+        title: 'Les Misérables',
+        genre: 'Historical Fiction',
+        author: 'Victor Hugo',
+        read: true
+    }
+];
+
+bookRouter.route('/')
+    .get(function (req, res) {
+        res.render('books',
+            {
+                title: 'Books',
+                nav: [
+                    { Link: '/Books', Text: 'Books' },
+                    { Link: '/Authors', Text: 'Authors' }
+                ],
+                books: books
+            }
+            );
+    });
+
+bookRouter.route('/single')
+    .get(function (req, res) {
+        res.send('Hello Single Book');
+    });
+
+module.exports = bookRouter;
