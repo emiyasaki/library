846b829af378e4f85797c489e04ffd6776c27390
Edmilson Miyasaki <edmilson.miyasaki@ilegra.com>
2016-01-07 15:15:20 -0200

    adding simple routing with text

--- app.js
+++ app.js
@@ -3,26 +3,39 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 5000;
+var bookRouter = express.Router();
 
 app.use(express.static('public'));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res){
-    res.render('index', 
-    {
-        title: 'Hello from EJS render', 
-        nav: [
-            {Link: '/Books', Text: 'Books'}, 
-            {Link: '/Authors', Text: 'Authors'}
-        ]
+bookRouter.route('/')
+    .get(function (req, res) {
+        res.send('Hello Books');
     });
+
+bookRouter.route('/single')
+    .get(function (req, res) {
+        res.send('Hello Single Book');
+    });
+
+app.use('/Books', bookRouter);
+
+app.get('/', function (req, res) {
+    res.render('index',
+        {
+            title: 'Hello from EJS render',
+            nav: [
+                { Link: '/Books', Text: 'Books' },
+                { Link: '/Authors', Text: 'Authors' }
+            ]
+        });
 });
 
-app.get('/books', function(req, res){
+app.get('/books', function (req, res) {
     res.send('Hello, books');
 });
 
-app.listen(port, function(err){
+app.listen(port, function (err) {
     console.log('running server on port ' + port);
 });
