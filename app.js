13ccf009108236729e74fc4963f69b43d790e3d5
Edmilson Miyasaki <edmilson.miyasaki@ilegra.com>
2016-01-08 15:17:36 -0200

    creating auth routes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
 
@@ -10,18 +11,23 @@ var nav = [
 ];
 var bookRouter = require('./src/routes/bookRoutes')(nav);
 var adminRouter = require('./src/routes/adminRoutes')(nav);
+var authRouter = require('./src/routes/authRoutes')(nav);
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 app.use('/Books', bookRouter);
 app.use('/Admin', adminRouter);
+app.use('/Auth', authRouter);
 
 app.get('/', function (req, res) {
     res.render('index',
         {
-            title: 'Hello from EJS render',
+            title: 'Library',
             nav: [
                 { Link: '/Books', Text: 'Books' },
                 { Link: '/Authors', Text: 'Authors' }
