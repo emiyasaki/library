052be8bbb86d8798baaa41bea4551d61137af380
Edmilson Miyasaki <edmilson.miyasaki@ilegra.com>
2016-01-08 13:56:29 -0200

    inserting data onto MongoDB

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,12 +1,49 @@
 var express = require('express');
 var adminRouter = express.Router();
+var mongodb = require('mongodb').MongoClient;
 
-var router = function(nav){
+var books = [
+        {
+            title: 'War and Peace',
+            genre: 'Historical Fiction',
+            author: 'Tolstoy',
+            read: false
+        },
+        {
+            title: 'Les Misérables',
+            genre: 'Historical Fiction',
+            author: 'Victor Hugo',
+            read: true
+        },
+        {
+            title: 'Life On The Mississipi',
+            genre: 'History',
+            author: 'Mark Twain',
+            read: false
+        },
+        {
+            title: 'The Wind in the Willows',
+            genre: 'Fantasy',
+            author: 'Kenneth Grahame',
+            read: false
+        }
+    ];
+
+var router = function (nav) {
     adminRouter.route('/addBooks')
-        .get(function(req, res){
-            res.send('inserting books');
+        .get(function (req, res) {
+            var url = 'mongodb://localhost:27017/libraryApp';
+            mongodb.connect(url, function (err, db) {
+                var collection = db.collection('books');
+                collection.insertMany(books,
+                    function (err, results) {
+                        res.send(results);
+                        db.close();
+                    });
+            });
+            //res.send('inserting books');
         });
-    
+
     return adminRouter;
 };
 
